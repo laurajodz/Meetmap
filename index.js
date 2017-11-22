e4dc2e21d283c101d599cd76b8078f8c1abe5ca6
Laura Jodz <laurajodz@gmail.com>
2017-11-21 23:22:54 -0800

    added hardcoded data to test

--- index.js
+++ index.js
@@ -258,7 +258,8 @@ map = new google.maps.Map(document.getElementById('map'), {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
-  }
+  showMarkers();
+}
 
   // function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   // infoWindow.setPosition(pos);
@@ -268,88 +269,98 @@ map = new google.maps.Map(document.getElementById('map'), {
   //   infoWindow.open(map);
   // }
 
-
-
-  var locations = [
-    {title: 'Meetup 1', location: {lat: 37.1100, lng: -95.7000}},
-    {title: 'Meetup 2', location: {lat: 37.0700, lng: -95.7000}},
-    {title: 'Meetup 3', location: {lat: 37.0001, lng: -95.7000}},
-    {title: 'Meetup 4', location: {lat: 37.0910, lng: -95.7100}},
-    {title: 'Meetup 5', location: {lat: 37.0900, lng: -95.7150}},
-    {title: 'Meetup 6', location: {lat: 37.1000, lng: -95.7200}}
+var locations = [
+    {title: 'Meetup 1', location: {lat: 34.0762, lng: -118.2637}},
+    {title: 'Meetup 2', location: {lat: 34.0760, lng: -118.2637}},
+    {title: 'Meetup 3', location: {lat: 34.0750, lng: -118.2600}},
+    {title: 'Meetup 4', location: {lat: 34.0781, lng: -118.2605}},
+    {title: 'Meetup 5', location: {lat: 34.0766, lng: -118.2637}},
+    {title: 'Meetup 6', location: {lat: 34.0700, lng: -118.2600}}
   ];
 
-  var largeInfowindow = new google.maps.InfoWindow();
-  //var bounds = new google.maps.LatLngBounds();
-
-  for (var i = 0; i < locations.length; i++) {
+function showMarkers() {
+  $('#btn').click(event => {
+    event.preventDefault();
+    var largeInfowindow = new google.maps.InfoWindow();
+    //var bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < locations.length; i++) {
     // Get the position from the location array.
-    console.log("hi");
-    var position = locations[i].location;
-    var title = locations[i].title;
+      var position = locations[i].location;
+      var title = locations[i].title;
     // Create a marker per location, and put into markers array.
-    var marker = new google.maps.Marker({
-      map: map,
-      position: position,
-      title: title,
-      animation: google.maps.Animation.DROP,
-      id: i
-    });
+      var marker = new google.maps.Marker({
+        map: map,
+        position: position,
+        title: title,
+        animation: google.maps.Animation.DROP,
+        id: i
+      });
+      // Push the marker to our array of markers
+      markers.push(marker);
+    }
+  });
+  //openWindow();
+}
 
-    // Push the marker to our array of markers.
-    markers.push(marker);
-    // Create an onclick event to open an infowindow at each marker.
-    marker.addListener('click', function() {
-      populateInfoWindow(this, largeInfowindow);
-    });
-    //bounds.extend(markers[i].position);
-        }
+// Create an onclick event to open an infowindow at each marker.
+// function openWindow(marker) {
+//   marker.addListener('click', function() {
+//     populateInfoWindow(this, largeInfowindow);
+//   });
+//   //bounds.extend(markers[i].position);
+// }
+
     // Extend the boundaries of the map for each marker
     //map.fitBounds(bounds);
 
     //This function populates the infowindow when the marker is clicked. We'll only allow
     //one infowindow which will open at the marker that is clicked, and populate based
     //on that markers position.
-    function populateInfoWindow(marker, infowindow) {
-      // Check to make sure the infowindow is not already opened on this marker.
-      if (infowindow.marker != marker) {
-        infowindow.marker = marker;
-        infowindow.setContent('<div>' + marker.title + '</div>');
-        infowindow.open(map, marker);
-        // Make sure the marker property is cleared if the infowindow is closed.
-        infowindow.addListener('closeclick',function(){
-          infowindow.setMarker = null;
-        });
-      }
-    }
+function populateInfoWindow(marker, infowindow) {
+  // Check to make sure the infowindow is not already opened on this marker.
+  if (infowindow.marker != marker) {
+    infowindow.marker = marker;
+    infowindow.setContent('<div>' + marker.title + '</div>');
+    infowindow.open(map, marker);
+    // Make sure the marker property is cleared if the infowindow is closed.
+    infowindow.addListener('closeclick',function(){
+      infowindow.setMarker = null;
+    });
+  }
+}
+
+// const queryTargetKeyword = $(event.currentTarget).find('#js-keyword');
+// const queryKeyword = queryTarget.val();
+// const queryTargetCategory = $(event.currentTarget).find('#js-keyword');
+// const queryCategory = queryTarget.val();
+
+
+function displayResults(data){
+    data.events.forEach(ev => {
+        $('.results').append(ev.description);
+    });
+}
 
 
 
-
-//
-// function displayResults(data){
-//     data.events.forEach(ev => {
-//         $('.results').append(ev.description);
-//     });
-// }
-//
-//
-// $(function(){
-//     $('#btn').click(function(e){
-//         const q = {
-//             key: "524472e125072465129556564d2f74",
-//             callback:"handlerequest"
-//         }
-//         $.ajax({
-//             url:'https://api.meetup.com/find/upcoming_events',
-//             method:'GET',
-//             data: q,
-//             dataType:"jsonp",
-//             crossDomain:true
-//         }).done(res => {
-//            displayResults(res.data);
-//         })
-//     });
-// })
+$(function(){
+    $('#btn').click(function(e){
+        const q = {
+            key: "524472e125072465129556564d2f74",
+            description: `${keyword}`,
+            
+            callback:"handlerequest"
+        }
+        $.ajax({
+            url:'https://api.meetup.com/find/upcoming_events',
+            method:'GET',
+            data: q,
+            dataType:"jsonp",
+            crossDomain:true
+        }).done(res => {
+           displayResults(res.data);
+        })
+    });
+})
 
 // map.data.loadGeoJson('http://www.CORS-ENABLED-SITE.com/data.json');
